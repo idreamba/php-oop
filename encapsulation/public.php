705ad72b8112223d74d42ec4664422eba1dea6b7
Komkrit Aree <idreamba@gmail.com>
2022-10-25 15:59:25 +0700

    Update /encapsulation/public.php

--- encapsulation/public.php
+++ encapsulation/public.php
@@ -1,5 +1,8 @@
 <?php
 
+// Report all PHP errors
+//error_reporting(E_ALL);
+
 /***
  * public เข้าถึงข้อมูลได้ทุกที่ไม่จำกัด
  * protected เข้าถึงได้ในคลาสแม่ และคลาสลูกที่ใช้การสืบทอดคุณสมบัติ (Inherite) เท่านั้น
@@ -11,11 +14,24 @@ class Employee{
 	// attribute
 	public $name;
 	public $department;
+	// ปรับเป็น private แล้ว object จะไม่สามารถแก้ไขข้อมูลได้ตรงๆ
+	//private $name;
 }
 
+// สร้าง object คนที่ 1 จากคลาส Employeee
 $emp1 = new Employee();
 $emp1->name = "Dream";
+// ถ้ากำหนด data แบบ public มันสามารถเข้ามาเปลี่ยนค่าได้เลย
+$emp1->name = "XXX";
 $emp1->department = "Product Service";
 
 echo "My name is ". $emp1->name."<br/>";
-echo "Department is ". $emp1->department."<br/>";
+echo "Department is ". $emp1->department."<hr/>";
+
+// สร้าง object คนที่ 2 จากคลาส Employeee
+$emp2 = new Employee();
+$emp2->name = "Som";
+$emp2->department = "IT Support";
+
+echo "My name is ". $emp2->name."<br/>";
+echo "Department is ". $emp2->department."<br/>";
